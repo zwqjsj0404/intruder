c5c25e1809549a8c738738e890f8bb2860701b49
longer <lwwcl1314@163.com>
2012-05-29 19:42:12 +0800

    intruder.sh  for linux

--- intruder-agent/src/main/resources/bin/intruder.sh
+++ intruder-agent/src/main/resources/bin/intruder.sh
@@ -1,7 +1,8 @@
-#! /bin/sh
+#!/bin/sh
 echo "Usage: intruder.sh <confPath> <pid>"
-echo 
-INTRUDER_HOME=`dirname "$0"`/..
-INTRUDER_HOME=`cd "${INTRUDER_HOME}" && pwd`
+echo
+INTRUDER_HOME_1=`dirname "$0"`/..
+INTRUDER_HOME=`cd ${INTRUDER_HOME_1} && pwd`
 CLIENT=${INTRUDER_HOME}/intruder-agent-0.1.jar
-java -cp ${CLIENT} com.alibaba.intruder.agent.core.Client ${CLIENT} $1 $2
+TOOLS_JAR="${JAVA_HOME}/lib/tools.jar"
+${JAVA_HOME}/bin/java -cp ${TOOLS_JAR}:${CLIENT} com.alibaba.intruder.agent.core.Client ${CLIENT} $1 $2
